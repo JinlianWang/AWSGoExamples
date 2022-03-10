6a68893a171927b04e8d467464022cd73aec4b0e
Sunny Wang <wangjinlian@hotmail.com>
2022-03-09 20:25:10 -0500

    Allow for assuming another role and then get caller identity - before clean up.

--- sts_assume_role.go
+++ sts_assume_role.go
@@ -11,42 +11,65 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/sts"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
 )
 
 // Usage:
 // go run sts_assume_role.go
 func main() {
-    // Initialize a session in us-west-2 that the SDK will use to load
-    // credentials from the shared credentials file ~/.aws/credentials.
-    sess, err := session.NewSession(&aws.Config{
-        Region: aws.String("us-west-2"),
-    })
+	// Initialize a session in us-west-2 that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials.
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
 
-    if err != nil {
-        fmt.Println("NewSession Error", err)
-        return
-    }
+	if err != nil {
+		fmt.Println("NewSession Error", err)
+		return
+	}
 
-    // Create a STS client
-    svc := sts.New(sess)
+	// Create a STS client
+	svc := sts.New(sess)
 
-    roleToAssumeArn := "arn:aws:iam::123456789012:role/roleName"
-    sessionName := "test_session"
-    result, err := svc.AssumeRole(&sts.AssumeRoleInput{
-        RoleArn:         &roleToAssumeArn,
-        RoleSessionName: &sessionName,
-    })
+	roleToAssumeArn := "arn:aws:iam::975156237701:role/CoS-Secrets-Provisioner"
+	sessionName := "test_session"
+	result, err := svc.AssumeRole(&sts.AssumeRoleInput{
+		RoleArn:         &roleToAssumeArn,
+		RoleSessionName: &sessionName,
+	})
 
-    if err != nil {
-        fmt.Println("AssumeRole Error", err)
-        return
-    }
+	if err != nil {
+		fmt.Println("AssumeRole Error", err)
+		return
+	}
 
-    fmt.Println(result.AssumedRoleUser)
+	fmt.Println("Assumed Credentials: ", result)
+
+	fmt.Println("Access ID: ", result.Credentials.AccessKeyId)
+
+	newSession, err := session.NewSession(&aws.Config{
+		Region:      aws.String("us-east-1"),
+		Credentials: credentials.NewStaticCredentials(*result.Credentials.AccessKeyId, *result.Credentials.SecretAccessKey, *result.Credentials.SessionToken),
+	})
+
+	if err != nil {
+		fmt.Println("NewSession Error", err)
+		return
+	}
+
+	newSVC := sts.New(newSession)
+	res, err := newSVC.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+
+	if err != nil {
+		fmt.Println("GetCallerIdentity Error", err)
+		return
+	}
+
+	fmt.Println("Identity: ", res)
+
 }
-
